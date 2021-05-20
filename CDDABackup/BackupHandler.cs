b664e3f347fb12f39dfb40207b02dae2c3bdde91
mGuv <mattmcg.gamedev@gmail.com>
2021-05-20 22:44:50 +0100

    Address PR changes

--- CDDABackup/BackupHandler.cs
+++ CDDABackup/BackupHandler.cs
@@ -40,14 +40,21 @@ namespace CDDABackup
         /// </summary>
         private readonly int timeBetweenUpdatesMilliseconds;
 
+        /// <summary>
+        /// Lookup for the last time a folder (the key) was modified (the value), used to control when to actually start making the backup
+        /// </summary>
         private Dictionary<string, DateTime> lastWritten = new Dictionary<string, DateTime>();
 
+        /// <summary>
+        /// The Application control so we can gracefully request shutdown via user input
+        /// </summary>
         private IHostApplicationLifetime hostApplicationLifetime;
 
         /// <summary>
         /// Creates a new Backup Handler with the given config
         /// </summary>
         /// <param name="config">The configuration the Handler will pull values from</param>
+        /// <param name="hostApplicationLifetime">The application lifetime to call shutdown on when user requests</param>
         /// <exception cref="ApplicationException">Thrown when the application is not configured correctly</exception>
         public BackupHandler(IConfiguration config, IHostApplicationLifetime hostApplicationLifetime)
         {
@@ -67,46 +74,12 @@ namespace CDDABackup
                     "Save directory has not been set! Please modify the appSettings.json to point 'saveDirectory' to your CDDA save directory.");
             }
         }
-
-        /// <summary>
-        /// Handler for when a file changes within a save directory
-        /// </summary>
-        /// <param name="sender">The event sender</param>
-        /// <param name="e">The event containing the file changed</param>
-        private void OnSaveChanged(object sender, FileSystemEventArgs e)
-        {
-            // Only mark actual saves
-            string path = e.FullPath;
-            if (path == this.backupDirectoryPath)
-            {
-                return;
-            }
-
-            // Note the time we saw this update
-            lastWritten[path] = DateTime.Now;
-        }
-
-        /// <summary>
-        /// Takes a given directory and writes an exact copy of all of its contents to the backup directory, recursively.
-        /// </summary>
-        /// <param name="sourceDirectory">The original Directory to Copy</param>
-        /// <param name="backupDirectory">The destination of the Copy</param>
-        private void BackupFolder(DirectoryInfo sourceDirectory, DirectoryInfo backupDirectory)
-        {
-            // Backup all the files
-            foreach (var file in sourceDirectory.GetFiles())
-            {
-                file.CopyTo(Path.Combine(backupDirectory.FullName, file.Name));
-            }
-
-            // Backup all the Directories
-            foreach (var directory in sourceDirectory.GetDirectories())
-            {
-                // Recursion isn't ideal but I highly doubt this will ever be nested enough to stack overflow
-                this.BackupFolder(directory, backupDirectory.CreateSubdirectory(directory.Name));
-            }
-        }
         
+        /// <summary>
+        /// Run the main backup tasks until cancelled
+        /// </summary>
+        /// <param name="stoppingToken">The token that the Handler will watch for when it should stop</param>
+        /// <returns>Indefinite backup task that only stops once the token has been cancelled</returns>
         private async Task RunBackups(CancellationToken stoppingToken)
         {
             try
@@ -175,7 +148,50 @@ namespace CDDABackup
             }
         }
 
-        protected  override async Task ExecuteAsync(CancellationToken stoppingToken)
+        /// <summary>
+        /// Handler for when a file changes within a save directory
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event containing the file changed</param>
+        private void OnSaveChanged(object sender, FileSystemEventArgs e)
+        {
+            // Only mark actual saves
+            string path = e.FullPath;
+            if (path == this.backupDirectoryPath)
+            {
+                return;
+            }
+
+            // Note the time we saw this update
+            lastWritten[path] = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Takes a given directory and writes an exact copy of all of its contents to the backup directory, recursively.
+        /// </summary>
+        /// <param name="sourceDirectory">The original Directory to Copy</param>
+        /// <param name="backupDirectory">The destination of the Copy</param>
+        private void BackupFolder(DirectoryInfo sourceDirectory, DirectoryInfo backupDirectory)
+        {
+            // Backup all the files
+            foreach (var file in sourceDirectory.GetFiles())
+            {
+                file.CopyTo(Path.Combine(backupDirectory.FullName, file.Name));
+            }
+
+            // Backup all the Directories
+            foreach (var directory in sourceDirectory.GetDirectories())
+            {
+                // Recursion isn't ideal but I highly doubt this will ever be nested enough to stack overflow
+                this.BackupFolder(directory, backupDirectory.CreateSubdirectory(directory.Name));
+            }
+        }
+        
+        /// <summary>
+        /// Wraps the backup task with control logic
+        /// </summary>
+        /// <param name="stoppingToken">The application wide stopping token</param>
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             // Write out fluffy intro
             Console.WriteLine("[CDDA Backup Tool]");
@@ -195,7 +211,6 @@ namespace CDDABackup
                     this.RunBackups(stoppingToken)
                 )
             );
-            
             
             Console.WriteLine("done");
         }
--- CDDABackup/Program.cs
+++ CDDABackup/Program.cs
@@ -6,13 +6,26 @@ using Serilog;
 
 namespace CDDABackup
 {
+    /// <summary>
+    /// Contains the entry point to the application and any bootstrap logic
+    ///
+    /// Driven entirely by the settings in appSettings.json
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main entry point to the program
+        /// </summary>
+        /// <returns>Task that upon completion the application haas ended</returns>
         static async Task Main()
         {
             await Program.CreateHostBuilder().RunConsoleAsync();
         }
-
+        
+        /// <summary>
+        /// Configures/builds the entire applications main service, including config, DI and logging
+        /// </summary>
+        /// <returns>The configured Host Builder ready for use</returns>
         private static IHostBuilder CreateHostBuilder()
         {
             return new HostBuilder()

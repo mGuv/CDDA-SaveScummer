3dd34cc4392319c0f9f177e5a80d8a5de75ddb08
mGuv <mattmcg.gamedev@gmail.com>
2021-05-22 11:49:09 +0100

    Remove unused usings

--- CDDABackup/Program.cs
+++ CDDABackup/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System.Threading.Tasks;
-using CDDABackup.FileHandling;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
